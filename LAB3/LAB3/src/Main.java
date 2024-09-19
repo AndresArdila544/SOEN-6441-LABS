24da4243f1984006b3b04c0f6dc887d53a310e6c
Andres Ardila <andres.ardila.agudelo@gmail.com>
2024-09-19 12:13:29 -0400

    lab3

---
+++ LAB3/LAB3/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public final int value = 10;
+
+    public void doIt() {
+        int value = 6;
+        Runnable r = new Runnable() {
+            public final int value = 5;
+            @Override
+            public void run() {
+                int value = 10;
+                System.out.println(this.value);
+            }
+        };
+        r.run();
+    }
+
+    public static void main(String... args) {
+        Main m = new Main();
+        m.doIt(); // ???
+    }
+
+}
